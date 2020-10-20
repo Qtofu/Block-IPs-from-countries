09db8ace346b92b4ad17f982528e3527312a624a
Qtofu <1052665606@qq.com>
2020-10-20 21:17:36 +0800

    Update block-ips.sh

--- block-ips.sh
+++ block-ips.sh
@@ -18,7 +18,7 @@ root_need(){
 block_ipset(){
 check_ipset
 #添加ipset规则
-echo -e "${Green}请输入需要封禁的国家代码，如cn(中国)，注意字母为小写！${Font}"
+echo -e "${Green}请输入禁止访问的国家代码，如cn(中国)，注意字母为小写！${Font}"
 read -p "请输入国家代码:" GEOIP
 echo -e "${Green}正在下载IPs data...${Font}"
 wget -P /tmp http://www.ipdeny.com/ipblocks/data/countries/$GEOIP.zone 2> /dev/null
@@ -36,8 +36,8 @@ for i in $(cat /tmp/$GEOIP.zone ); do ipset -A $GEOIP $i; done
 rm -f /tmp/$GEOIP.zone
 echo -e "${Green}规则添加成功，即将开始封禁ip！${Font}"
 #开始封禁
-iptables -I INPUT -p tcp -m set --match-set "$GEOIP" src -j DROP
-iptables -I INPUT -p udp -m set --match-set "$GEOIP" src -j DROP
+iptables -I OUTPUT -p tcp -m set --match-set "$GEOIP" src -j DROP
+iptables -I OUTPUT -p udp -m set --match-set "$GEOIP" src -j DROP
 echo -e "${Green}所指定国家($GEOIP)的ip封禁成功！${Font}"
 }
 
@@ -48,10 +48,10 @@ read -p "请输入国家代码:" GEOIP
 #判断是否有此国家的规则
 lookuplist=`ipset list | grep "Name:" | grep "$GEOIP"`
     if [ -n "$lookuplist" ]; then
-        iptables -D INPUT -p tcp -m set --match-set "$GEOIP" src -j DROP
-	iptables -D INPUT -p udp -m set --match-set "$GEOIP" src -j DROP
+        iptables -D OUTPUT -p tcp -m set --match-set "$GEOIP" src -j DROP
+	iptables -D OUTPUT -p udp -m set --match-set "$GEOIP" src -j DROP
 	ipset destroy $GEOIP
-	echo -e "${Green}所指定国家($GEOIP)的ip解封成功，并删除其对应的规则！${Font}"
+	echo -e "${Green}所禁止访问的国家($GEOIP)的ip解封成功，并删除其对应的规则！${Font}"
     else
 	echo -e "${Green}解封失败，请确认你所输入的国家是否在封禁列表内！${Font}"
 	exit 1
@@ -99,10 +99,10 @@ root_need
 check_release
 clear
 echo -e "———————————————————————————————————————"
-echo -e "${Green}Linux VPS一键屏蔽指定国家所有的IP访问${Font}"
+echo -e "${Green}Linux VPS一键禁止访问指定国家所有的IP${Font}"
 echo -e "${Green}1、封禁ip${Font}"
 echo -e "${Green}2、解封iP${Font}"
-echo -e "${Green}3、查看封禁列表${Font}"
+echo -e "${Green}3、查看禁止访问列表${Font}"
 echo -e "———————————————————————————————————————"
 read -p "请输入数字 [1-3]:" num
 case "$num" in
